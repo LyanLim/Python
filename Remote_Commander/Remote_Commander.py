5118ac78f1109cfec6d09265b3b0fb1acc6e2e04
LyanLim <hellolcs@castis.com>
2016-07-27 14:12:02 +0900

    - 연결, 파일전송, 실행부분에 대한 proto type 완료 - cfg 파일 생성
    ; cfg read&parsing 부분 추가 필요
    ; 네트워크 연결 및 실행부분에 대한 예외 처리 필요

--- Remote_Commander/Remote_Commander.py
+++ Remote_Commander/Remote_Commander.py
@@ -2,37 +2,34 @@
 
 from fabric.api import *
 
-def deploy(ip, password, user='root'):
+def deploy(ip, password, user, src_full_path, dst_path):
     with settings(
-            hide('warnings', 'running', 'stdout'),
-            #parallel = True,
-            host_string=ip,
-            user = user,
-            password = password,
-            cwd = '/home/'
-            #warn_only = True
-        ):
-            result = put('filelist.txt', '/home/').succeeded
+        hide('warnings', 'running', 'stdout'),
+        #parallel = True,
+        host_string=ip,
+        user = user,
+        password = password,
+    ):
+        result = put(src_full_path, dst_path).succeeded
 
     return result
 
 
-def remote_commander(ip, password, user='root'):
-    with settings(
-            hide('warnings', 'running', 'stdout'),
-            #parallel = True,
-            host_string=ip,
-            user = user,
-            password = password,
-            cwd = '/home/'
-            #warn_only = True
-        ):
-            # with prefix("hostname"), prefix('echo "test : "'):
+def remote_commander(ip, password, user, dst_full_path, script_name):
 
-            with cd('/home'):
-                 with quiet():
-                     result = run('ls `cat filelist.txt`').succeeded
-                    # result = run('bash test.sh')
+    command = 'ls `cat ' + dst_full_path + '/' + script_name + '`'
+    with settings(
+        hide('warnings', 'running', 'stdout'),
+        #parallel = True,
+        host_string=ip,
+        user = user,
+        password = password,
+    ):
+
+        with cd(dst_full_path):
+            with quiet():
+                result = run(command).succeeded
+
     return result
 
 
@@ -44,17 +41,24 @@ def main():
         '1.255.85.213'  :   'tbdev12!(',
         '1.255.85.214'  :   'tbdev12!(',
         '1.255.85.215'  :   'tbdev12!(',
-        '1.255.85.219'  :   'tbdev12!(',
+        '1.255.85.219'  :   'tbdev12!('
     }
 
     for ip in ip_list.keys():
-        if deploy(ip, ip_list[ip]):
-            print ("%s :: %s" % (ip, remote_commander(ip, ip_list[ip])))
+        if deploy(ip, ip_list[ip], user_id, src_full_path, dst_path):
+            print ("remote_commander,%s,%s" % (ip, remote_commander(ip, ip_list[ip], user_id, dst_full_path, 'filelist1.txt')))
         else:
             print ("%s, File Transfer Fail" % ip)
 
 
 if __name__ == '__main__':
-    main()
+    user_id='root'
+    src_path = './'
+    src_dir_name='transfer'
+    dst_path='/home'
+    src_full_path = src_path + src_dir_name
+    dst_full_path = dst_path + '/' + src_dir_name
 
+    # start!!
+    main()
 
