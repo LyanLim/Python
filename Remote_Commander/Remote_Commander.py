9a505e6c25d38beb841ed53d30074bd61ae92ea2
LyanLim <hellolcs@castis.com>
2016-08-05 11:33:59 +0900

    Remote_Commander.cfg 파일 로딩 함수 완료
    > Remote Excute 부분 예외 처리 필요
    > Remote Excute On/Off 부분 작성 필요

--- Remote_Commander/Remote_Commander.py
+++ Remote_Commander/Remote_Commander.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import re
 from fabric.api import *
 
 def deploy(ip, password, user, src_full_path, dst_path):
     with settings(
         hide('warnings', 'running', 'stdout'),
-        #parallel = True,
+        # parallel = True,
         host_string=ip,
         user = user,
         password = password,
     ):
+
         result = put(src_full_path, dst_path).succeeded
 
     return result
 
 
-def remote_commander(ip, password, user, dst_full_path, script_name):
-
-    command = 'ls `cat ' + dst_full_path + '/' + script_name + '`'
+def remote_commander(ip, password, user, dst_full_path, script_name, command):
     with settings(
         hide('warnings', 'running', 'stdout'),
-        #parallel = True,
+        # parallel = True,
         host_string=ip,
         user = user,
         password = password,
@@ -32,33 +32,53 @@ def remote_commander(ip, password, user, dst_full_path, script_name):
 
     return result
 
+def get_cfg():
+    f = open("Remote_Commander.cfg", "r")
+    lines = f.readlines()
+    for line in lines:
+        if re.match('^#', line[0]):
+            pass
+        else:
+            line = line.strip('\n')
+            key = line.split('=')
+            if re.match('Target_Information', key[0]):
+                value = key[1].split(';')
+                ip_list[value[0]] = [value[1], value[2]]
+            else:
+                config[key[0]] = key[1]
+
+    f.close()
 
 def main():
-    ip_list = {
-        '1.255.85.210'  :   'dev123$%^',
-        '1.255.85.211'  :   'tbdev12!(',
-        '1.255.85.212'  :   'tbdev12!(',
-        '1.255.85.213'  :   'tbdev12!(',
-        '1.255.85.214'  :   'tbdev12!(',
-        '1.255.85.215'  :   'tbdev12!(',
-        '1.255.85.219'  :   'tbdev12!('
-    }
-
     for ip in ip_list.keys():
-        if deploy(ip, ip_list[ip], user_id, src_full_path, dst_path):
-            print ("remote_commander,%s,%s" % (ip, remote_commander(ip, ip_list[ip], user_id, dst_full_path, 'filelist1.txt')))
+
+        if config['Excute_Mode']:
+            if deploy(ip, ip_list[ip][1], ip_list[ip][0], src_full_path, dst_path):
+                print ("deploy,%s,\"%s\" to \"%s\" upload,success" % (ip, src_full_path, dst_path))
+                command = 'bash ' + dst_full_path + '/' + start_sctipt
+                print ("remote_commander,%s,\"%s\" excute,%s" % (ip, command, remote_commander(ip, ip_list[ip], ip_list[ip][0], dst_full_path, start_sctipt, command)))
+            else:
+                print ("%s, File Transfer Fail" % ip)
         else:
-            print ("%s, File Transfer Fail" % ip)
-
-
+            pass
 if __name__ == '__main__':
-    user_id='root'
-    src_path = './'
-    src_dir_name='transfer'
-    dst_path='/home'
-    src_full_path = src_path + src_dir_name
-    dst_full_path = dst_path + '/' + src_dir_name
 
-    # start!!
+    config = {}
+    ip_list = {}
+
+    #read config file
+    get_cfg()
+
+    src_path = config['Source_Dir_Path']
+    src_dir_name= config['Source_Dir_Name']
+    dst_path = config['Destination_Path']
+    src_full_path = src_path + '/' + src_dir_name
+    dst_full_path = dst_path + '/' + src_dir_name
+    if int(config['Excute_Mode']):
+        if config.get('Start_Sctipt'):
+            start_sctipt = config['Start_Sctipt']
+        else:
+            exit("You must check config file : Start_Sctipt")
+
     main()
 
